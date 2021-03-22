4109efade15c447575c1c435a6839e536f0ef262
BenReichwein <ben.basque@icloud.com>
2021-03-21 20:36:28 -0400

    More landing page changes

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -26,7 +26,7 @@ export default function Landing() {
                 <p className='textP'>
                     If you ever come into a situation where you need immediate help from people around you but you can't reach them by yelling?
                     Safety Net is an app that alerts everyone in a 200m radius around you. It will show them your exact location and a description
-                    of you so you can get the immediate help you need.
+                    of you so you can get the immediate help you need. <span style={{color:'red'}}>(This isn't a replacement for 911)</span>
                 </p>
                 <img className="frame2" src={frame2} alt="Frame2" />
             </div>
